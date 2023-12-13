8a16aff1d0174ac5136943f735dc5dc928c57618
Nathalia <1903606@cmontmorency.qc.ca>
2023-12-13 15:17:01 -0500

    FAIT: Initiale

---
+++ database/migrations/2023_12_13_191858_create_trips_table.php
@@ -0,0 +1,35 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreateTripsTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('trips', function (Blueprint $table) {
+            $table->id();
+            $table->string('country');
+            $table->text('title');
+            $table->text('story'); 
+            $table->string('traveller');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('trips');
+    }
+}
